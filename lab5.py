316b379afe6d7928a11d4bbb8b6870ceec99dceb
annalisebuonya <abuonya@calpoly.edu>
2024-10-28 12:06:37 -0700

    update 2 to task 3

--- lab5.py
+++ lab5.py
@@ -16,6 +16,9 @@ def time_add(time1: Time, time2: Time):
     sum_of_hours = time1.hour + time2.hour
     sum_of_minutes = time1.minute + time2.minute
     sum_of_seconds = time1.second + time2.second
+
+    if sldkfsdjfsdlkjf'sdjlsg;sgjasljfsljgsdljglsa'
+
     sum_of_times = Time(sum_of_hours, sum_of_minutes, sum_of_seconds)
     return sum_of_times
 
--- lab5_tests.py
+++ lab5_tests.py
@@ -1,6 +1,7 @@
-import data
 import lab5
 import unittest
+from data import Time
+
 
 
 # Write your test cases for each part below.
@@ -14,7 +15,12 @@ class TestCases(unittest.TestCase):
 
 
     # Part 3
-
+    def test_time_add1(self):
+        time1 = Time(4,50,55)
+        time2 = Time(7,25,23)
+        result = lab5.time_add(time1, time2)
+        expected = Time(11, 75, 78)
+        self.assertEqual(expected, result)
 
 
     # Part 4
